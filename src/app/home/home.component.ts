d3b549ecea23af633b4530f141c4e701c825f0da
Tushar <tushar.mohan2001@gmail.com>
2020-07-11 15:14:35 +0530

    bubble sort done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChildren , ViewChild, QueryList} from '@angular/core';
 import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 import { timer } from 'rxjs';
 import { ConstantPool } from '@angular/compiler';
@@ -9,71 +9,105 @@ import { ConstantPool } from '@angular/compiler';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  array:Array<number>;
+  array;
   list;
   i=1;
   k=0;
   len;
   key;
-  constructor() { }
+  @ViewChild('ba') ba:ElementRef;
+  @ViewChildren('av') av:QueryList<ElementRef>;
+  @ViewChild('h3') h3:ElementRef;
+  @ViewChild('h4') h4:ElementRef;
+  constructor(
+    public element:ElementRef
+  ) { }
 
   ngOnInit(): void {
     this.list = [];
     for(var i =0 ;i<=3;i++){
       this.list.push(Math.floor(Math.random()*10+1));
     }
-    // this.list.forEach(ele=>{console.log(ele);});
-    // // this.list=list;
-    // var x = 0;
     this.len=this.list.length;
     this.key=this.list[1];
   }
+  ngAfterViewInit(){
+    console.log(this.ba.nativeElement);
+    // this.av.nativeElement[0].classList.add('red');
+    // console.log(this.av.nativeElement[0].classList);
+    // this.av.nativeElement.classList.remove('blue');
+    // console.log(this.av.nativeElement.classList);
+    console.log(this.av);
+    this.array=this.av.toArray();
+    console.log(this.array[0].nativeElement.classList);
+  }
+  bubbleUtil=()=>{
+    this.i=0;
+    this.len=this.list.length;
+    // console.log(this.a.nativeElement);
+    this.h3.nativeElement.classList.remove('none');
+    this.h3.nativeElement.classList.add('none');
+    this.bubbleSort();
+  }
+  insertionUtil=()=>{
+    this.i=1;
+    this.k=0;
+    this.key=this.list[1];
+    this.h3.nativeElement.classList.remove('none');
+    this.h4.nativeElement.classList.remove('none');
+    this.h3.nativeElement.classList.add('none');
+    this.h4.nativeElement.classList.add('displayDiv');
+    this.insertionSort();
+  }
   bubbleSort=()=>{
-    var element=document.getElementsByTagName('div');
-    element[this.i].setAttribute("style","background-color:red");
-    if(this.i<this.len)
-      element[this.i+1].setAttribute("style","background-color:red");
-    if(this.list[this.i]>this.list[this.i+1]){
-      [this.list[this.i],this.list[this.i+1]]=[this.list[this.i+1],this.list[this.i]];
-      // element[this.i+1].setAttribute("style","background-color:red");
-    }
-    this.i++;
     if(this.i>=this.len){
-    //   for(var j=this.len;j>=this.i;j--)
-        element[this.len-1].setAttribute("style","background-color:green");
+      this.array[this.len-1].nativeElement.classList.remove('red');
+      this.array[this.len-1].nativeElement.classList.add('green');
+      for(var j=0;j<this.list.length;j++)
+        this.array[this.len-1].nativeElement.classList.remove('red');
       this.len--;
       this.i=0;
     }
     if(this.len<=0){
-      for(var j=1;j<this.list.length;j++)
-        element[j].setAttribute("style","background-color:green");
+      this.h3.nativeElement.classList.remove('none');
+      this.h3.nativeElement.classList.remove('displayDiv');
       return;
     }
-    else{
-      for(var j=1;j<this.list.length;j++)
-        element[j].setAttribute("style","background-color:black");
-      element[this.len-1].setAttribute("style","background-color:green");
-      setTimeout(this.bubbleSort,300);
+    if(this.list[this.i]>this.list[this.i+1]){
+      this.array[this.i].nativeElement.classList.remove('red');
+      this.array[this.i+1].nativeElement.classList.remove('red');
+      this.array[this.i].nativeElement.classList.add('red');
+      this.array[this.i+1].nativeElement.classList.add('red');
+      [this.list[this.i],this.list[this.i+1]]=[this.list[this.i+1],this.list[this.i]];
+      this.array[this.i].nativeElement.classList.remove('red');
     }
+    this.i++;
+    setTimeout(this.bubbleSort,300);
   };
   insertionSort=()=>{
-    console.log(this.i+" heha");
-    this.list.forEach(ele => {
-      console.log(ele);
-    });
-    if(this.i==this.list.length)
+    var element=document.getElementsByTagName('div');
+    if(this.i==this.list.length){
+      for(var o=0;o<element.length;o++)
+        element[o].setAttribute("style","background-color:green");
       return;
+    }
     if(this.k>=0 && this.list[this.k]>this.key){
       this.list[this.k+1]=this.list[this.k];
       this.k--;
-      console.log(this.k);
     }
     else {
       this.list[this.k+1]=this.key;
       this.i++;
+      if(this.i==this.list.length){
+        for(var o=0;o<this.list.length;o++)
+          element[o].setAttribute("style","background-color:green");
+        return;
+      }
       this.k=this.i-1;
       this.key=this.list[this.i];
+      console.log(parseInt(element[this.i].innerHTML)==4 );
+      element[this.i].setAttribute("style","background-color:red;");
     }
-    setTimeout(this.insertionSort,10);
+    setTimeout(this.insertionSort,1000);
   };
 }
